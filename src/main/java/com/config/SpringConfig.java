97365894626275ec6843805092bc48b8b0795b2f
Kilipurt <proandrey15@gmail.com>
2018-10-17 22:43:29 +0300

    Servlet java-based config

--- src/main/java/com/config/SpringConfig.java
+++ src/main/java/com/config/SpringConfig.java
@@ -1,11 +1,13 @@
-package com.util;
+package com.config;
 
 import com.lesson2.OrderDAO;
 import com.lesson2.OrderService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com")
 public class SpringConfig {
 
     @Bean
